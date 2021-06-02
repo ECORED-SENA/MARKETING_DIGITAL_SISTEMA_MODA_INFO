b4c8cbe9052578d9979cbdbfdf153f23ccd86eee
scamacho84 <sergio.camachoo@misena.edu.co>
2021-06-02 09:23:20 -0500

    Avance 1

---
+++ src/views/Inicio.vue
@@ -0,0 +1,152 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p Apreciado aprendiz, bienvenido al curso Marketing digital para el sistema moda, una experiencia de aprendizaje que se inmiscuye en el mundo comercial, comunicativo y de las formas de vestir. Con la intención de reconocer el contexto de aprendizaje, observe el siguiente video.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación<br>del programa
+
+    p.mb-4 Las empresas del sector moda, como industria que crece día a día, requieren de una constante actualización, con el fin de adaptar sus procesos a los retos que implican los nuevos tiempos. Por ello, se ha hecho necesario buscar diversas alternativas para generar desarrollo con los elementos que se tiene al alcance.
+
+    p.mb-4 Con la convergencia de medios en esta era digital y la rápida evolución y obsolescencia de la tecnología, se han generado nuevas maneras de concebir, crear, distribuir, comercializar y consumir los contenidos y productos propios de este sector. La interactividad que actualmente existe entre el público y los contenidos, que en los medios análogos tradicionales no se manifestaba de manera tan libre e inmediata, ahora, en los medios digitales, es una nueva posibilidad que las industrias creativas están llamadas a integrar en sus diferentes procesos.
+
+    p Con el fin de mejorar la competitividad de la industria nacional a través de un mejor posicionamiento en el mercado, es como se hace necesaria la formación en la incorporación, uso y seguimiento del marketing digital, pensado desde el contexto empresarial, el cual será la herramienta para generar valor y, a su vez, transformación social y productiva.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Competencias<br>a desarrollar
+
+    ul.lista-ul.mb-4
+      li.d-flex
+        i.fas.fa-angle-right
+        | Aplicar prácticas de protección ambiental, seguridad y salud en el trabajo de acuerdo con las políticas organizacionales y la normatividad vigente.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Atender clientes de acuerdo con procedimiento de servicio y normativa.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Definir alternativas de diseño según necesidades del cliente y condiciones de la empresa.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Desarrollar procesos de comunicación eficaces y efectivos, teniendo en cuenta situaciones de orden social, personal y productivo.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Ejercer derechos fundamentales del trabajo en el marco de la constitución política y los convenios internacionales.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Interactuar en el contexto productivo y social de acuerdo con principios éticos para la construcción de una cultura de paz.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Estructurar la estrategia de mercadeo digital de acuerdo con objetivos de campaña y normativa legal.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Generar hábitos saludables de vida mediante la aplicación de programas de actividad física en los contextos productivos y sociales.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Gestionar procesos propios de la cultura emprendedora y empresarial de acuerdo con el perfil personal y los requerimientos de los contextos productivo y social.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Interactuar en la lengua inglesa de forma oral y escrita, dentro de contextos sociales y laborales, según los criterios establecidos por el Marco Común Europeo de Referencia para las Lenguas (MCERL).
+      li.d-flex
+        i.fas.fa-angle-right
+        | Identificar la dinámica organizacional del SENA.
+      li.d-flex
+        i.fas.fa-angle-right
+        | Utilizar herramientas informáticas de acuerdo con las necesidades de manejo de información.
+
+    //- p
+    //-   span.colorText01
+    //-     strong 220601041.
+    //-   | Implementar sistemas de gestión según normativa y requerimientos técnicos.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Perfil<br>de ingreso
+
+    p El programa está dirigido a toda la población que se desenvuelve en el sector moda y tiene la necesidad de desarrollar habilidades para el marketing digital.
+      br
+      | La persona que ingresa al programa debe contar con certificación académica de grado noveno de bachillerato, la edad mínima requerida por la ley es de 14 años. De igual manera, el aspirante se debe desenvolver en los tres saberes: cognitivo (saber saber), aptitudinal, destrezas y habilidades (saber hacer) y personal (saber ser).
+
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Perfil<br>de egreso
+
+    p El egresado de este programa será responsable de apoyar a los administradores del proceso de marketing digital en el manejo y la construcción de las comunidades online en empresas del sector moda, a partir de la identidad e imagen de la marca, el conocimiento del producto y las relaciones con los clientes y seguidores de las redes sociales, de forma ética y responsable.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 6. Estrategia<br>metodológica
+
+    p.mb-4 La estrategia metodológica está centrada en la construcción de autonomía para garantizar la calidad en el marco de la formación por competencias, el aprendizaje por proyectos, el uso de herramientas digitales y la aplicación de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales. Estas actividades están soportadas en la utilización de las Tecnologías de la Información y la Comunicación (TIC), integradas en ambientes virtuales de aprendizaje, que, en todo caso, recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias.
+
+    p.mb-4 Igualmente, se estimula de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    .row.g-4.justify-content-center
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/01.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El instructor - tutor
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/02.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El entorno
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/03.svg', alt='El instructor - tutor')
+        p.text-center
+          strong Las TIC
+      .col-5.col-sm-3.col-md-2
+        figure.mb-2
+          img.w-75.mx-auto(src='@/assets/curso/04.svg', alt='El instructor - tutor')
+        p.text-center
+          strong El trabajo colaborativo
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
